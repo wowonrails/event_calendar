014bf91268a6eeb013f90c2c0283e9e4a0505bb1
wowonrails <wowaonrails@gmail.com>
2017-10-09 14:05:13 +0300

    Added models, configured routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,49 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable
+  paginates_per 20
+
+  devise :database_authenticatable,
+         :registerable,
+         :confirmable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   validates :full_name, presence: true
+
+  has_many :events, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id",
+                           dependent: :destroy
+
+  has_many :followed_users, through: :relationships,
+                            source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+
+  has_many :followers, through: :reverse_relationships,
+                       source: :follower
+
+  def self.other_users(user)
+    ids = all.pluck(:id)
+    rejected_ids = (
+      user.followed_users.pluck(:id) + user.followers.pluck(:id)
+    ).push(user.id)
+     .uniq
+
+    where(id: ids.reject{ |id| rejected_ids.include?(id) })
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  resources :events
+
+  resources :users, only: :show do
+    resources :followed_users, only: :index
+    resources :followers, only: :index
+    resources :other_users, only: :index
+  end
+
+  resources :relationships, only: [:create, :destroy]
 end
